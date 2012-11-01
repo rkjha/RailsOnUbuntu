c8fce181b4e3ec9f83d82b678e292c3b56a46bbd
Ramesh Jha <rameshjha420@gmail.com>
2012-11-01 18:20:56 +0530

    adds nodejs ppa to rails installer and updates readme

--- rails-installer.sh
+++ rails-installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# A simple Script for installing Rails on Ubuntu 12.04 LTS
+# A simple Script for installing Rails on Ubuntu 12.04 LTS / 12.10
 # It will also install the dependencies required as well as the RVM
-# Author: Ramesh Jha (rameshjha420@gmail.com),(http://blog.sudobits.com)
+# Author: Ramesh Jha (ramesh@rameshjha.com),(http://blog.sudobits.com)
 # License: MIT 
 
 RUBY_VERSION="1.9.3"
@@ -40,7 +40,12 @@ echo "...........Done..........."
 
 # Install Additional Dependencies
 
-sudo apt-get -y install build-essential openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion nodejs
+sudo apt-get -y install build-essential openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
+
+# Install nodejs from the PPA
+sudo apt-add-repository -y ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get -y install nodejs
 
 # Install Latest version of Ruby
 echo "........Installing Ruby v $RUBY_VERSION ..."
